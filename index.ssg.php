06284cb74217a8fbfaecffa2e61de4a3fe8340c8
jhs512 <jangka512@gmail.com>
2021-09-27 16:44:34 +0900

    세팅

---
+++ index.ssg.php
@@ -0,0 +1,4 @@
+<?php $appCode = "index"; ?>
+<?php require_once "head.php"; ?>
+인덱스
+<?php require_once "foot.php"; ?>
---
+++ productList.ssg.php
@@ -0,0 +1,4 @@
+<?php $appCode = "productList"; ?>
+<?php require_once "head.php"; ?>
+상품리스트
+<?php require_once "foot.php"; ?>
